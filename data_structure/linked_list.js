abf74183bd666816c6f1ed91a5b8f8f57a322a2c
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2019-06-20 10:19:19 +0530

    Added Unit Test Cases

---
+++ data_structure/linked_list.js
@@ -0,0 +1,34 @@
+class node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+        this.tail = null;
+    }
+
+    push(value) {
+
+    }
+
+    pop() {
+
+    }
+
+    get (index) {
+
+    }
+
+    delete(index) {
+
+    }
+
+    _find(value) {
+
+    }
+}
